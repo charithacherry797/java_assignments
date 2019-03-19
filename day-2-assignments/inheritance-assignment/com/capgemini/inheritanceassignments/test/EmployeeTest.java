0070bcfc530a53809422240aafe535e1ba7d47c3
tamatam <charitha.tamatam@capgemini.com>
2019-03-19 18:23:25 +0530

    first commit

---
+++ day-2-assignments/inheritance-assignment/com/capgemini/inheritanceassignments/test/EmployeeTest.java
@@ -0,0 +1,16 @@
+package com.capgemini.inheritanceassignments.test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class EmployeeTest {
+
+	
+	Employee1 e1 = new Employee1();
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
---
+++ inheritance/src/com/capgemini/inheritance/main/MarketingExecutive.java
@@ -0,0 +1,29 @@
+package com.capgemini.inheritance.main;
+
+public class MarketingExecutive extends Employee {
+	private double kilometersTravelled;
+	private double tourAllowance;
+	private static final double  telephoneAllowance = 1500;
+	
+	
+	public MarketingExecutive() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+	
+	public MarketingExecutive(int employeeId,String employeeName,double basicSalary,double medical,double kilometersTravelled) {
+		super();
+		this.kilometersTravelled = kilometersTravelled;
+		
+	}
+
+	public double grossSalary() {
+		tourAllowance=kilometersTravelled*5;
+		return super.grossSalary()+telephoneAllowance+tourAllowance;
+	}
+	public double netSalary() {
+		return super.netsalary();
+	}
+}
+
+
